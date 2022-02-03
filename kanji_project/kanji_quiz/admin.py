0e2331a92c8783206b5a05653d39e1ad4bb97115
Vaduva <petremarian.vaduva@dxc.com>
2022-02-03 23:49:36 +0000

    update admin interface

--- kanji_project/kanji_quiz/admin.py
+++ kanji_project/kanji_quiz/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Kanji, Portuguese
 # Register your models here.
 
+class KanjiAdmin(admin.ModelAdmin):
+    list_display = ('character', 'level', 'is_number', 'is_radical')
+    list_filter = ('level', 'is_number', 'is_radical')
 
+class PortugueseAdmin(admin.ModelAdmin):
+    list_display = ('meaning', 'example')
 
-
-admin.site.register(Kanji)
-admin.site.register(Portuguese)
+admin.site.register(Kanji, KanjiAdmin)
+admin.site.register(Portuguese, PortugueseAdmin)
