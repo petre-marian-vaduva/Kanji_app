5cdbe9658edc0a84ee4567669fa5c0abb63a4602
Vaduva <petremarian.vaduva@dxc.com>
2022-02-04 14:11:17 +0000

    add paths and views

--- kanji_project/kanji_quiz/views.py
+++ kanji_project/kanji_quiz/views.py
@@ -1,8 +1,16 @@
+from wsgiref.util import request_uri
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Kanji, Portuguese
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello there')
+    all_kanji = Kanji.objects.all()
+    return render(request, 'kanji_quiz/index.html', {
+        'all_kanji': all_kanji
+    })
+
+def level1(request, slug):
+    kanji_level = Kanji.objects.get(pk=slug)
+    return render(request, 'kanji_quiz/level1.html')
