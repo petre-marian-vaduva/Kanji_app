d526da738a34ed98880a730c3ca9ef26e2c70fa8
Vaduva <petremarian.vaduva@dxc.com>
2022-02-05 00:50:45 +0000

    last update

--- kanji_project/kanji_quiz/urls.py
+++ kanji_project/kanji_quiz/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<slug>', views.level1, name='level1')
+    path('<slug>/', views.level, name='level'),
+    path('<slug>/', views.kanji_detail, name='kanji_detail')
 ]
--- kanji_project/kanji_quiz/views.py
+++ kanji_project/kanji_quiz/views.py
@@ -11,6 +11,32 @@ def index(request):
         'all_kanji': all_kanji
     })
 
-def level1(request, slug):
-    kanji_level = Kanji.objects.get(pk=slug)
-    return render(request, 'kanji_quiz/level1.html')
+def level(request, slug):
+    all_kanji = Kanji.objects.all()
+    if slug == 'level1': 
+        return render(request, 'kanji_quiz/level1.html', {
+            'all_kanji': all_kanji
+        })
+    elif slug == 'level2': 
+        return render(request, 'kanji_quiz/level2.html', {
+            'all_kanji': all_kanji
+        })
+    elif slug == 'level3': 
+        return render(request, 'kanji_quiz/level3.html', {
+            'all_kanji': all_kanji
+        })
+    elif slug == 'level4': 
+        return render(request, 'kanji_quiz/level4.html', {
+            'all_kanji': all_kanji
+        })
+    elif slug == 'level5': 
+        return render(request, 'kanji_quiz/level5.html', {
+            'all_kanji': all_kanji
+        })
+
+def kanji_detail(request, slug):
+    kanji = Kanji.objects.get(pk=slug)
+    kanji_meaning = kanji.portuguese_set.all()
+    return render(request, 'kanji_quiz/kanji_detail.html', {
+        'kanji_detail': kanji_detail
+    })
