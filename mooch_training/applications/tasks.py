a50eedc12a69a3451a07d3bf97f5ced326637360
Petre <81445716+petre-marian-vaduva@users.noreply.github.com>
2022-04-19 22:23:23 +0100

    Update tasks.py

--- mooch_training/applications/tasks.py
+++ mooch_training/applications/tasks.py
@@ -1,8 +1,7 @@
-from celery import shared_task
+# from celery import shared_task
 import subprocess, sys
-from scheduler.models import Order, Runtime, Jobs, Tasks
-import subprocess, sys
-import time
+# from scheduler.models import Order, Runtime, Jobs, Tasks
+# import time
 # @shared_task(name="sum_two_numbers")
 # def add(x, y):
 #     return x + y
@@ -11,33 +10,33 @@ import time
 
 
 
-@shared_task(name="script1")
-def script_test():
-    args = ["powershell.exe", "C:\\test\\change_status.ps1"]
-    p=subprocess.Popen(args,
-        stdout=sys.stdout)
+# @shared_task(name="script1")
+# def script_test():
+args = ["python.exe", "C:\\keys\\test.py"]
+p=subprocess.Popen(args,
+    stdout=sys.stdout)
 
 
 
-@shared_task(name="script2")
-def change_status():
-    status_obj = Runtime.objects.filter(status='test')
-    if status_obj:
-        for row in status_obj:     
-            row.status = 'Initialized'
-            id_number = row.job_id
-            job_obj = Order.objects.filter(job_id=id_number)
-            task_name = job_obj[0].task
-            task_order = job_obj[0].order 
-            row.order = task_order
-            current_job = row.job.job
-            row.save()
-            begin_script_path = 'C:\\test\\'
-            folder = current_job
-            script_path = begin_script_path + folder + '\\' + task_name
-            time.sleep(5)
-            args = ["powershell.exe", script_path]
-            p=subprocess.Popen(args, stdout=sys.stdout)
+# @shared_task(name="script2")
+# def change_status():
+#     status_obj = Runtime.objects.filter(status='test')
+#     if status_obj:
+#         for row in status_obj:     
+#             row.status = 'Initialized'
+#             id_number = row.job_id
+#             job_obj = Order.objects.filter(job_id=id_number)
+#             task_name = job_obj[0].task
+#             task_order = job_obj[0].order 
+#             row.order = task_order
+#             current_job = row.job.job
+#             row.save()
+#             begin_script_path = 'C:\\test\\'
+#             folder = current_job
+#             script_path = begin_script_path + folder + '\\' + task_name
+#             time.sleep(5)
+#             args = ["powershell.exe", script_path]
+#             p=subprocess.Popen(args, stdout=sys.stdout)
             
 
             # if row.status == 'Job requested':
